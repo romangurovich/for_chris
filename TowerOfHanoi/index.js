0da98e5e790a62b5baf0c71322a221b340639ef0
Roman Gurovich <romangurovich@gmail.com>
2014-12-01 22:28:14 -0800

    Recursive hanoi

--- TowerOfHanoi/index.js
+++ TowerOfHanoi/index.js
@@ -1,4 +1,20 @@
 var actions = require("./actions");
 var config = require("./config");
 
+var moveOnePiece = function(from, to) {
+    actions.move(from,to);
+};
+
+var moveNPieces = function(n,from, to, via) {
+    if (n=== 1) {
+        moveOnePiece(from, to)
+    } else {
+        moveNPieces(n - 1, from, via, to);
+        moveOnePiece(from, to);
+        moveNPieces(n-1,via, to, from);
+    }
+};
+
+moveNPieces(config.pieces, 0, 2, 1);
+
 actions.finish();
